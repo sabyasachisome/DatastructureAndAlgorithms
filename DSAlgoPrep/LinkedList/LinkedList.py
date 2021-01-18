cc675ac40a25c2ac951dd1b4e380c5f32d2b90f9
sabyasachi <sabyasachisome123@gmail.com>
2021-01-18 11:18:30 +0530

    adding insert method to linked list

--- DSAlgoPrep/LinkedList/LinkedList.py
+++ DSAlgoPrep/LinkedList/LinkedList.py
@@ -1,44 +1,43 @@
-"""
-Linked List
-"""
 class Node:
     def __init__(self, data):
-        self.data= data
+        self.data=data
         self.next= None
-
+        
 class LinkedList:
     def __init__(self):
         self.head= None
-
-    def printList(self):
+        
+    def insert_node(self, val, pos):
+        target= Node(val)
+        if pos==0:
+            target.next= self.head
+            self.head= target
+            return
+        else:
+            previous_node= self.get_value(pos-1)
+            target.next= previous_node.next
+            previous_node.next= target
+            print('After insert')
+            self.print_list()
+    
+    def get_value(self, pos):
+        counter=0 #head starts=0, counter= 1 if head starts=1
         temp= self.head
-        linked_list="head=> "
-        while(temp.next):
-            linked_list+= str(temp.data)+" => "
-            temp= temp.next
-        linked_list+= str(temp.data)+""
-        print(linked_list)
-
-    def getPrevious(self, pos):
-        temp= self.head
-        counter=1
         while(counter<pos):
             temp= temp.next
             counter+=1
+        print(temp.data)
         return temp
-
-    def insertNode(self, value, pos):
-        target= Node(value)
-        if (pos==0):
-            target.next= self.head
-            self.head= target
-        else:
-            previous= self.getPrevious(pos)
-            nextNode= previous.next
-
-            previous.next= target
-            target.next= nextNode
-
+    
+    def print_list(self):
+        temp= self.head
+        list_str= "head=>"
+        while(temp.next):
+            list_str+= str(temp.data)+"=>"
+            temp= temp.next
+        list_str+= str(temp.data)
+        print(list_str)
+        
 # create the linkedList
 linked_list= LinkedList()
 
@@ -57,13 +56,8 @@ node3.next=node4
 # print data
 linked_list.printList()
 
-# insert at 0
-linked_list.insertNode(10,0)
-linked_list.printList()
-
-# insert at other than 0
-linked_list.insertNode(11,2)
-linked_list.printList()
-
-linked_list.insertNode(12,1)
-linked_list.printList()
+# consider head starts at 0
+# get value at position
+linked_list.get_value(3)
+#insert node
+linked_list.insert_node(6,2)
