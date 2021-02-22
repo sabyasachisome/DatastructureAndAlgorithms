074b3b09f7d792c5e9b0fca33fc11cd1ed6e8d5d
sabyasachi <sabyasachisome123@gmail.com>
2021-02-22 11:06:48 +0530

    arrays and strings: 1.2 move zeroes- using 2 pointer logic to avoid taking extra array

--- DSAlgoPrep/ArrayAndString/move_zeroes.py
+++ DSAlgoPrep/ArrayAndString/move_zeroes.py
@@ -2,18 +2,13 @@ from typing import List
 
 class MoveZeroes:
     def move_zeroes(self, nums: List[int]):
-        count=0
-        new_arr=[]
-        for i in range(len(nums)):
-            if nums[i]==0:
-                count+=1
+        j: int= 0
         for i in range(len(nums)):
             if nums[i]!=0:
-                new_arr.append(nums[i])
-        for i in range(count):
-            new_arr.append(0)
-        nums= new_arr
-        print(nums)
+                nums[j]=nums[i]
+                j+=1
+        for i in range(j, len(nums)):
+            nums[i]=0
 
 arr=[0,1,0,3,12]
 move_zero= MoveZeroes()
