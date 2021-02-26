80f738b93513ac56dbee9a6e9c168a48dcd85f4b
sabyasachi <sabyasachisome123@gmail.com>
2021-02-26 13:48:53 +0530

    StacksAndQueues example 1: Stack example

---
+++ DSAlgoPrep/StacksAndQueues/1_stack.py
@@ -0,0 +1,18 @@
+class PlateStack:
+    def __init__(self):
+        self.st=[]
+
+    def push(self):
+        self.st.append(x)
+
+    def pop(self):
+        if len(self.st)>0:
+            self.st.pop()
+
+    def top(self):
+        if len(self.st)==0:
+            return None
+        return self.st[-1]
+
+    def get_len(self):
+        return len(self.st)
