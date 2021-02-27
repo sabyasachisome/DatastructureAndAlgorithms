7029d59187855b2d12a08660d830b2f3785ea121
sabyasachi <sabyasachisome123@gmail.com>
2021-02-27 21:54:41 +0530

    modyfying sorting file

--- Sortings/Sorting.py
+++ Sortings/Sorting.py
@@ -69,7 +69,7 @@ def partition(arr, low, high):
     pivot= arr[high]
     i=low-1
     for j in range(low, high):
-        if arr[low]<pivot:
+        if arr[j]<pivot:
             i+=1
             arr[i],arr[j]=arr[j],arr[i]
     arr[i+1],arr[high]= arr[high], arr[i+1]
