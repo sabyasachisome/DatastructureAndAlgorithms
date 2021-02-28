0ae6fbe47086507013dbd1ec8edd076e66b7938a
sabyasachi <sabyasachisome123@gmail.com>
2021-02-28 23:04:00 +0530

    Array problem 5: adding problem description

--- DSAlgoPrep/ArrayAndString/5_longest_substring_without_repeating_characters.py
+++ DSAlgoPrep/ArrayAndString/5_longest_substring_without_repeating_characters.py
@@ -1,3 +1,11 @@
+"""
+Given a string s, find the length of the longest substring without repeating characters.
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+"""
+
 strn= 'geeksforgeeks'
 """
 sliding wondow technique
