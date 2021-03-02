ff5507f70f2bc00d9e9a5377dbacfd73d6e3633d
sabyasachi <sabyasachisome123@gmail.com>
2021-03-02 18:52:26 +0530

    Graphs: updating directed graph: adjacency matrix method

--- DatastructureAndAlgorithms/8_Graphs/2_directed_graph_adjacency_matrix.py
+++ DatastructureAndAlgorithms/8_Graphs/2_directed_graph_adjacency_matrix.py
@@ -4,6 +4,27 @@ class Graph:
         self.graph= [[0 for x in range(no_of_nodes+1)]
                      for y in range(no_of_nodes+1)]
 
-    def insert_node(self, v1, v2):
+    def insert_edge(self, v1, v2):
         self.graph[v1][v2]=1
-        
+
+    def within_bounds(self, v1, v2):
+        return (v1>=0 and v1<=self.no_of_nodes and v2>=0 and v2<=self.no_of_nodes)
+
+    def print_graph(self):
+        print('As Adjacency Matrix')
+        for i in range(len(self.graph)):
+            print(self.graph[i])
+
+        print('As graph')
+        for i in range(self.no_of_nodes):
+            for j in range(len(self.graph[i])):
+                if self.graph[i][j]:
+                    print(i,'->',j)
+
+g= Graph(5)
+g.insert_edge(1,2)
+g.insert_edge(1,3)
+g.insert_edge(2,3)
+g.insert_edge(4,5)
+
+g.print_graph()
