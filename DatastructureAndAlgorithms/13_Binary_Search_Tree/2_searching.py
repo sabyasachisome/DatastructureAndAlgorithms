91b8ae261a5d904fe492b2ce1618ab3322f11c7a
sabyasachi <sabyasachisome123@gmail.com>
2021-03-15 13:17:44 +0530

    Binary Search Tree: Modifying Searching

--- DatastructureAndAlgorithms/13_Binary_Search_Tree/2_searching.py
+++ DatastructureAndAlgorithms/13_Binary_Search_Tree/2_searching.py
@@ -22,6 +22,17 @@ def insert_node(root: Node, node: Node):
         else:
             insert_node(root.left, node)
 
+def search_node(node: Node, key: Node):
+    if node.data is None:
+        return None
+    print("Current node is", node.data)
+    if node.data==key:
+        print('node found')
+        return node
+    if node.data<key:
+        return search_node(node.right, key)
+    return search_node(node.left, key)
+
 def preorder(node):
     if node:
         print(node.data)
@@ -33,3 +44,5 @@ insert_node(tree, Node(6))
 insert_node(tree, Node(20))
 insert_node(tree, Node(1))
 preorder(tree)
+print('Starting search')
+print(search_node(tree, 6).data)
