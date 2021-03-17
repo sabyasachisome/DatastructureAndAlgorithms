c5c5fee2991ce61b47cea6264747eddc49b21455
sabyasachi <sabyasachisome123@gmail.com>
2021-03-17 23:39:41 +0530

    Dijkstra: Updating/11_Di

--- DatastructureAndAlgorithms/11_DijkstraAlgorithm/dijkstra.py
+++ DatastructureAndAlgorithms/11_DijkstraAlgorithm/dijkstra.py
@@ -9,5 +9,41 @@ graph={
 'h':{'g':2}
 }
 
+graph= {
+'a':{'b':10,'c':3},
+'b':{'c':1,'d':2},
+'c':{'b':4,'d':8,'e':2},
+'d':{'e':7},
+'e':{'d':9}
+}
+
+
+
 def dijkstra(graph, start, goal):
-    pass
+    shortest_distance={}
+    predecessor={}
+    unseen_nodes=graph
+    infinity=9999999
+    path=[]
+    for node in unseen_nodes:
+        shortest_distance[node]= infinity
+    shortest_distance[start]= 0
+    # print(shortest_distance)
+
+    while unseen_nodes:
+        min_node= None
+        for node in unseen_nodes:
+            if min_node is None:
+                min_node= node
+            elif shortest_distance[node]< shortest_distance[min_node]:
+                min_node= node
+
+        for child_node, weight in graph[min_node].items():
+            # print(child_node,weight)
+            if shortest_distance[min_node]+weight<shortest_distance[child_node]:
+                shortest_distance[child_node]= shortest_distance[min_node]+weight
+                predecessor[child_node]= min_node
+        unseen_nodes.pop(min_node)
+    print(shortest_distance)
+
+dijkstra(graph, 'a','d')
