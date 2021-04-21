7c6fb140cfa64114489cf7c330cc5c05ccf851ad
Sabyasachi <sabyasachisome123@gmail.com>
2021-04-22 00:06:57 +0530

    9.DepthFirstSearch: Check if graph is connected- modifications

--- DatastructureAndAlgorithms/9_DepthFirstSearch/2_check_if_graph_is_connected.py
+++ DatastructureAndAlgorithms/9_DepthFirstSearch/2_check_if_graph_is_connected.py
@@ -1,37 +1,49 @@
 """
 Given a directed graph. The task is to check if the given graph is connected or not.
 """
+
 from collections import defaultdict
 
 class Graph:
     def __init__(self):
-        self.graph1= defaultdict(list)
-        self.graph2 = defaultdict(list)
+        self.graph= defaultdict(list)
 
     def insert_edge(self, v1, v2):
-        self.graph1[v1].append(v2)
-        self.graph2[v2].append(v1)
+        self.graph[v1].append(v2)
 
-    def dfs(self, start_node, graph):
-        visited_set= set()
+    def dfs(self, start_node):
+        self.visited_set= set()
         st=[]
         st.append(start_node)
 
         while(len(st)):
             cur_node = st[-1]
             st.pop()
-            if cur_node not in visited_set:
-                print(cur_node)
-                visited_set.add(cur_node)
+            if cur_node not in self.visited_set:
+                # print(cur_node)
+                self.visited_set.add(cur_node)
 
             for neighbour in self.graph[cur_node]:
-                if neighbour not in visited_set:
+                if neighbour not in self.visited_set:
                     st.append(neighbour)
 
-g= Graph()
-g.insert_edge(0,1)
-g.insert_edge(0,3)
-g.insert_edge(1,2)
-g.insert_edge(2,3)
-g.insert_edge(1,3)
-g.dfs(0, g.graph1)
+class Solution:
+    def check_connected(self, set1, set2):
+        if len(set1.difference(set2)) ==0 and len(set2.difference(set1))==0:
+            return True
+        return False
+
+if __name__=='__main__':
+    g= Graph()
+    g.insert_edge(0,1)
+    g.insert_edge(0,3)
+    g.insert_edge(1,2)
+    g.insert_edge(2,3)
+    g.insert_edge(1,3)
+    g.dfs(0)
+
+    list_of_nodes= [0,1,0,3,1,2,2,3,1,3]
+    list_of_distinct_nodes= set(list_of_nodes)
+
+    sol= Solution()
+    print(sol.check_connected(list_of_distinct_nodes, g.visited_set))
