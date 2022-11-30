7652d3b58840b4856f74a98426a5255f381822bb
sabyasachisome <sabyasachisome123@gmail.com>
2022-11-30 23:37:44 +0530

    Array problem- 1. remove element, 2. Search insert position

--- DatastructureAndAlgorithms/LeetCode/LeetcodeDaily.py
+++ DatastructureAndAlgorithms/LeetCode/LeetcodeDaily.py
@@ -74,3 +74,115 @@ class Solution:
 
 sol= Solution()
 sol.removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+
+"""
+#### 27. Remove Element
+Easy
+4.8K
+6.5K
+Companies
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+"""
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        elem_id=0
+        for idx in range(len(nums)):
+            if nums[idx]!= val:
+                nums[elem_id]= nums[idx]
+                elem_id+=1
+#         print(nums)
+        return elem_id
+
+sol= Solution()
+sol.removeElement([3,2,2,3],3)
+
+"""
+#### 35. Search Insert Position
+Easy
+10.7K
+504
+Companies
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+"""
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start= 0
+        end= len(nums)
+        while end>=start:
+            middle= start+ (end-start)//2
+            if nums[middle]==target:
+                return middle
+            elif nums[middle]>target:
+                end= middle-1
+            elif nums[middle]<=target:
+                start= middle+1
+        return -1
+    
+sol= Solution()
+sol.searchInsert([1,3,5,10],11)
