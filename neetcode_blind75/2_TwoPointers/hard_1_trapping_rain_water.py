840d22d5eb2df10625e06b80c3fc167d43cc8c70
sabyasachisome <sabyasachisome123@gmail.com>
2023-11-01 21:59:20 +0530

    modifying 2_TwoPointers/hard_1_trapping_rain_water.py

--- neetcode_blind75/2_TwoPointers/hard_1_trapping_rain_water.py
+++ neetcode_blind75/2_TwoPointers/hard_1_trapping_rain_water.py
@@ -24,9 +24,30 @@ n == height.length
 0 <= height[i] <= 105
 """
 from typing import List
+
 class Solution:
     def trap(self, height: List[int])-> int:
-        pass
+        left_max_height=[]
+        right_max_height=[]
+        max_height_arr=[]
+        for idx in range(len(height)):
+            if idx==0:
+                left_max_height.append(0)
+            else:
+                left_max_height.append(max(height[:idx]))
+        print(left_max_height)
+        for idx in range(len(height)):
+            if idx==len(height)-1: 
+                right_max_height.append(0)
+            else:
+                right_max_height.append(max(height[idx+1:]))
+        print(right_max_height)
+        for idx in range(len(height)):
+            if min(left_max_height[idx], right_max_height[idx])- height[idx]<0:
+                max_height_arr.append(0)
+            else:
+                max_height_arr.append(min(left_max_height[idx], right_max_height[idx])- height[idx])
+        return sum(max_height_arr)
 
 sol = Solution()
-sol.trap([0,1,0,2,1,0,1,3,2,1,2,1])
+print(sol.trap([4,2,0,3,2,5]))
